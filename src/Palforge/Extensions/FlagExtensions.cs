4f324baad4ef476c90877d2e13674b17ba4a9173
Aerafal <jimmy.mll@hotmail.com>
2026-07-26 23:18:45 +0200

    feat: add FlagExtensions for enum flag utility methods

---
+++ src/Palforge/Extensions/FlagExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Palforge.Extensions;
+
+internal static class FlagExtensions
+{
+    extension<T>(T enumFlag)
+        where T : unmanaged, Enum
+    {
+        public bool HasAnyFlag(params T[] flags)
+        {
+            return flags.Any(flag => enumFlag.HasFlag(flag));
+        }
+    }
+}
