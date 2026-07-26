7e21ec4c3b5b930f8ba5325e28f82cdab3bdef79
Aerafal <jimmy.mll@hotmail.com>
2026-07-26 22:51:51 +0200

    feat: add DllMain entry point for Palforge runtime initialization

---
+++ src/Palforge/DllMain.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Palforge;
+
+internal static class DllMain
+{
+    [UnmanagedCallersOnly]
+    public static void Main()
+    {
+        try
+        {
+        }
+        catch (Exception exception)
+        {
+            Environment.FailFast("Palforge runtime failed to start", exception);
+        }
+    }
+}
