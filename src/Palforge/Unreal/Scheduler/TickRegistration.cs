86b1a97c84fe269ac0156627720bfa33fe072872
Aerafal <jimmy.mll@hotmail.com>
2026-07-27 01:09:11 +0200

    feat: add TickRegistration for managing tick callbacks and disposal

---
+++ src/Palforge/Unreal/Scheduler/TickRegistration.cs
@@ -0,0 +1,21 @@
+﻿using Palforge.Unreal.Runtime;
+
+namespace Palforge.Unreal.Scheduler;
+
+internal sealed class TickRegistration : IDisposable
+{
+    private readonly UnrealRuntime _runtime;
+    private Action? _callback;
+
+    internal TickRegistration(UnrealRuntime runtime, Action callback)
+    {
+        _runtime = runtime;
+        _callback = callback;
+    }
+
+    public void Dispose()
+    {
+        if (Interlocked.Exchange(ref _callback, null) is { } callback)
+            _runtime.RemoveTickCallback(callback);
+    }
+}
