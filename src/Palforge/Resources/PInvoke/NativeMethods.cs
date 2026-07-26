0b78109972ddcb9c2557daf29aa0628a0174aab2
Aerafal <jimmy.mll@hotmail.com>
2026-07-26 22:49:00 +0200

    chore: add Palforge project

---
+++ src/Palforge/Resources/PInvoke/NativeMethods.cs
@@ -0,0 +1,12 @@
+﻿namespace Windows.Win32;
+
+internal static partial class PInvoke
+{
+    internal const uint GENERIC_READ = 2147483648U;
+
+    internal const uint GENERIC_WRITE = 1073741824U;
+
+    internal const string CONOUT_FILE = "CONOUT$";
+
+    internal const string NULL_FILE = "NUL";
+}
