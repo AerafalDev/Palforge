000d1ba715ffcc956d9174f39ff9b8bb998e5b68
Aerafal <jimmy.mll@hotmail.com>
2026-07-27 01:24:44 +0200

    feat: add UnrealRuntimeOptions for configuring runtime behaviors

---
+++ src/Palforge/Unreal/Runtime/UnrealRuntimeOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace Palforge.Unreal.Runtime;
+
+internal sealed class UnrealRuntimeOptions
+{
+    public const string SectionName = "Unreal";
+
+    public required bool DumpObjects { get; set; }
+
+    public required bool DumpActors { get; set; }
+
+    public required bool GenerateSdk { get; set; }
+}
