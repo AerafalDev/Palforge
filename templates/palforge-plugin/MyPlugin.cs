8e9a68bf5b083d2bb714a63d443ef9d374eb2111
Aerafal <jimmy.mll@hotmail.com>
2026-08-01 00:22:17 +0200

    refactor: rename lifecycle methods in MyPlugin template

--- templates/palforge-plugin/MyPlugin.cs
+++ templates/palforge-plugin/MyPlugin.cs
@@ -6,15 +6,11 @@ namespace MyPlugin;
 [Plugin("com.exemple.exempleplugin")]
 public sealed class MyPlugin : Plugin
 {
-    public override void OnLoad()
+    public override void OnStart()
     {
     }
 
-    public override void OnUpdate(float deltaSeconds)
-    {
-    }
-
-    public override void OnUnload()
+    public override void OnStop()
     {
     }
 }
