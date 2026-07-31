5bcc725e7297c3233b39f7ae0d7c2198ba8eab65
Aerafal <jimmy.mll@hotmail.com>
2026-07-31 22:20:08 +0200

    feat: add SelfCheck and SdkOutput properties to Unreal config and options

--- src/Palforge/Unreal/Runtime/UnrealRuntimeOptions.cs
+++ src/Palforge/Unreal/Runtime/UnrealRuntimeOptions.cs
@@ -4,9 +4,13 @@ internal sealed class UnrealRuntimeOptions
 {
     public const string SectionName = "Unreal";
 
+    public required bool SelfCheck { get; set; }
+
     public required bool DumpObjects { get; set; }
 
     public required bool DumpActors { get; set; }
 
     public required bool GenerateSdk { get; set; }
+
+    public required string SdkOutput { get; set; }
 }
