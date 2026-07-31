af71de71a231042746ddf9a29c7368fbbb4a38be
Aerafal <jimmy.mll@hotmail.com>
2026-07-31 10:06:33 +0200

    feat: add CommandOptions class and config section for command settings

---
+++ src/Palforge/Commands/Options/CommandOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Palforge.Commands.Options;
+
+internal sealed class CommandOptions
+{
+    public const string SectionName = "Commands";
+
+    public required string Prefix { get; set; }
+
+    public required string SenderName { get; set; }
+}
