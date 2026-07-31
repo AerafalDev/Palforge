8eadfd5f17a330fb0694001e327e931ba1b18b61
Aerafal <jimmy.mll@hotmail.com>
2026-07-31 09:55:18 +0200

    feat: add base classes for command context and modules

---
+++ src/Palforge/Commands/Modules/CommandContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Palforge.Commands.Modules;
+
+public abstract class CommandContext
+{
+    public abstract bool IsAdministrator { get; }
+
+    public abstract string Input { get; }
+
+    public abstract void Reply([StringSyntax(StringSyntaxAttribute.CompositeFormat)] string message, params object?[] args);
+}
---
+++ src/Palforge/Commands/Modules/CommandModule.cs
@@ -0,0 +1,6 @@
+﻿namespace Palforge.Commands.Modules;
+
+public abstract class CommandModule
+{
+    internal abstract void SetContext(CommandContext context);
+}
